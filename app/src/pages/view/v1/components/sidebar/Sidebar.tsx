24c0534a45fad3cca10def742eafa19b7b833154
Trent <mr.peschke@gmail.com>
2025-09-12 08:04:08 -0600

    adding a button to remind me to do a bit more functionality

--- app/src/pages/view/v1/components/sidebar/Sidebar.tsx
+++ app/src/pages/view/v1/components/sidebar/Sidebar.tsx
@@ -10,6 +10,7 @@ export default function Sidebar({}: Props) {
             <button><i className="fa-solid fa-pen-nib"></i> Edit</button>
             <button><i className="fa-solid fa-download"></i> Download</button>
             <button><i className="fa-solid fa-mask"></i> Download as Pregen</button>
+            <button><i className="fa-solid fa-eye"></i> Show Quick Edit Locations</button>
         </div>
     )
 }
