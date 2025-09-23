a9aa51c7aa64f314bca0d6ff1d3a500c210be36a
Trent <mr.peschke@gmail.com>
2025-09-23 17:15:50 -0600

    set up vitality and nerve die and mins

--- app/src/pages/view/v1/components/pageOne/components/rightColumn/components/favorVitalityNRanges/VitalityNNerveCalcDisplay.tsx
+++ app/src/pages/view/v1/components/pageOne/components/rightColumn/components/favorVitalityNRanges/VitalityNNerveCalcDisplay.tsx
@@ -1,30 +1,50 @@
 import { VitalityNNerveCalcInfo } from '@vault/common/interfaces/v1/pageOne/rightColumnInterfaces'
 import './FavorVitalityNRanges.css'
+import { useContext } from 'react'
+import EditingContext from '../../../../../../contexts/EditingContext'
 
 interface Props {
     vitalityNNerveCalcInfo: VitalityNNerveCalcInfo
 }
 
 export default function VitalityNNerveCalcDisplay({ vitalityNNerveCalcInfo }: Props) {
+    const isEditing = useContext(EditingContext)
+    
     const { vitalityDie, minVitality, nerveDie, minNerve } = vitalityNNerveCalcInfo
 
     return (
         <div className='v-n-n-calc-display-shell'>
             <span>
                 <strong>Vitality Die</strong>
-                <p>{vitalityDie}</p>
+                {isEditing ?
+                    <input value={vitalityDie} />
+                    :
+                    <p>{vitalityDie}</p>
+                }
             </span>
             <span>
                 <strong>Min Vitality</strong>
-                <p>{minVitality}</p>
+                {isEditing ?
+                    <input value={minVitality} />
+                    :
+                    <p>{minVitality}</p>
+                }
             </span>
             <span>
                 <strong>Nerve Die</strong>
-                <p>{nerveDie}</p>
+                {isEditing ?
+                    <input value={nerveDie} />
+                    :
+                    <p>{nerveDie}</p>
+                }
             </span>
             <span>
                 <strong>Min Nerve</strong>
-                <p>{minNerve}</p>
+                {isEditing ?
+                    <input value={minNerve} />
+                    :
+                    <p>{minNerve}</p>
+                }
             </span>
         </div>
     )
