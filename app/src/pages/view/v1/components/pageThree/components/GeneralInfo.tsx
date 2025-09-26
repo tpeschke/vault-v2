110bdf73cc470c172292a25cb60d1b9beaab64ac
Trent <mr.peschke@gmail.com>
2025-09-26 11:11:26 -0600

    finished up the editing page three styling

--- app/src/pages/view/v1/components/pageThree/components/GeneralInfo.tsx
+++ app/src/pages/view/v1/components/pageThree/components/GeneralInfo.tsx
@@ -1,17 +1,37 @@
 import { GeneralNotesInfo } from '@vault/common/interfaces/v1/pageThree/generalNotesInterfaces'
 import TextArea from '../../../../../../components/textArea/TextArea'
 import '../PageThree.css'
+import { useContext } from 'react'
+import EditingContext from '../../../contexts/EditingContext'
 
 interface Props {
     generalNotes: GeneralNotesInfo
 }
 
 export default function GeneralInfoDisplay({ generalNotes }: Props) {
-    const { notes } = generalNotes
+    const isEditing = useContext(EditingContext)
+
+    const { notes, isSecret } = generalNotes
+
+    const tooltip = isSecret ? "Only You Can View These Notes" : "Anyone Can View These Notes"
 
     return (
         <>
-            <h2>General Info</h2>
+            <span className='general-info-heading'>
+                <h2>General Info</h2>
+                {isEditing &&
+                    <span>
+                        <p>Is Secret?</p>
+                        <div className='fake-button' data-tooltip-id="my-tooltip" data-tooltip-content={tooltip}>
+                            {isSecret ?
+                                <i className="fa-solid fa-eye-slash" ></i>
+                                :
+                                <i className="fa-solid fa-eye"></i>
+                            }
+                        </div>
+                    </span>
+                }
+            </span>
             <TextArea lines={53} value={notes} />
         </>
     )
