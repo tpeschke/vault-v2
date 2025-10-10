0de8595948ab9271053418268681bae4d75c09fa
Trent <mr.peschke@gmail.com>
2025-10-10 08:54:18 -0600

    armor modifier total now updates when you update modifiers

--- app/src/pages/view/v1/hooks/utilities/updateUtilities/pageTwoUtilities/combatUtilities/armorUtilities.ts
+++ app/src/pages/view/v1/hooks/utilities/updateUtilities/pageTwoUtilities/combatUtilities/armorUtilities.ts
@@ -1,5 +1,6 @@
 import { CharacterVersion1 } from "@vault/common/interfaces/characterInterfaces";
 import { ArmorInfoObjectKeys, ArmorModifiersInfoKeys, ArmorModifiersObjectKeys } from "@vault/common/interfaces/v1/pageTwo/combatInterfaces/armorInterfaces";
+import { calculateArmorDefenseTotal, calculateArmorFatigueTotal, calculateArmorRecoveryOrInitiativeTotal } from "@vault/common/utilities/v1/armorUtilities";
 
 export function updateBasicArmorInfoUtility(character: CharacterVersion1, key: ArmorInfoObjectKeys, value: string | number) {
     return {
@@ -18,6 +19,33 @@ export function updateBasicArmorInfoUtility(character: CharacterVersion1, key: A
 }
 
 export function updateArmorModifierUtility(character: CharacterVersion1, modifier: ArmorModifiersInfoKeys, key: ArmorModifiersObjectKeys, value: number) {
+    let newModifiers = {
+        ...character.pageTwoInfo.combatWorkspaceInfo.armorInfo.modifiers,
+        [modifier]: {
+            ...character.pageTwoInfo.combatWorkspaceInfo.armorInfo.modifiers[modifier],
+            [key]: value
+        }
+    }
+
+    newModifiers = {
+        def: {
+            ...newModifiers.def,
+            total: calculateArmorDefenseTotal(newModifiers.def.base, newModifiers.def.skill, newModifiers.def.misc)
+        },
+        fat: {
+            ...newModifiers.fat,
+            total: calculateArmorFatigueTotal(newModifiers.fat.base, newModifiers.fat.skill, newModifiers.fat.misc)
+        },
+        rec: {
+            ...newModifiers.rec,
+            total: calculateArmorRecoveryOrInitiativeTotal(newModifiers.rec.base, newModifiers.rec.skill, newModifiers.rec.misc)
+        },
+        init: {
+            ...newModifiers.init,
+            total: calculateArmorRecoveryOrInitiativeTotal(newModifiers.init.base, newModifiers.init.skill, newModifiers.init.misc)
+        }
+    }
+
     return {
         ...character,
         pageTwoInfo: {
@@ -26,13 +54,7 @@ export function updateArmorModifierUtility(character: CharacterVersion1, modifie
                 ...character.pageTwoInfo.combatWorkspaceInfo,
                 armorInfo: {
                     ...character.pageTwoInfo.combatWorkspaceInfo.armorInfo,
-                    modifiers: {
-                        ...character.pageTwoInfo.combatWorkspaceInfo.armorInfo.modifiers,
-                        [modifier]: {
-                            ...character.pageTwoInfo.combatWorkspaceInfo.armorInfo.modifiers[modifier],
-                            [key]: value
-                        }
-                    }
+                    modifiers: newModifiers
                 }
             }
         }
--- backend/common/utilities/v1/armorUtilities.ts
+++ backend/common/utilities/v1/armorUtilities.ts
@@ -8,15 +8,19 @@ export function calculateArmorDefenseTotal(base: number = 0, skill: number = 0, 
     }
 }
 
-export function calculateArmorFatigueTotal(base: string = 'B', skill: number = 0, misc: number = 0): number {
-    const fatigueDictionary: {[key: string]: number} = {
-        'C': 0,
-        'W': 1,
-        'B': 2,
-        'H': 3
-    }
-    const total = fatigueDictionary[base] - Math.floor(skill / 2) + misc
+export function calculateArmorFatigueTotal(base: string | number = 'B', skill: number = 0, misc: number = 0): number {
+    if (typeof base === 'string') {
+        const fatigueDictionary: { [key: string]: number } = {
+            'C': 0,
+            'W': 1,
+            'B': 2,
+            'H': 3
+        }
 
+        base = fatigueDictionary[base]
+    }
+
+    const total = base - Math.floor(skill / 2) + misc
     if (total <= 0) {
         return total
     } else {
