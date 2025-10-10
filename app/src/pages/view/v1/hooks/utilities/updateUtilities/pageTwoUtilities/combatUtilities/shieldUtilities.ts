2453a79dc321554dcc2641e04910a0907f271250
Trent <mr.peschke@gmail.com>
2025-10-10 08:49:03 -0600

    shield modifier total updates when modifiers do

--- app/src/pages/view/v1/hooks/utilities/updateUtilities/pageTwoUtilities/combatUtilities/shieldUtilities.ts
+++ app/src/pages/view/v1/hooks/utilities/updateUtilities/pageTwoUtilities/combatUtilities/shieldUtilities.ts
@@ -1,5 +1,6 @@
 import { CharacterVersion1 } from "@vault/common/interfaces/characterInterfaces";
 import { ShieldInfoObjectKeys, ShieldModifiersInfoKeys, ShieldModifiersObjectKeys } from "@vault/common/interfaces/v1/pageTwo/shieldInterfaces";
+import { calculateShieldDefense, calculateShieldFatigue, calculateShieldParry, calculateShieldBreakage } from '@vault/common/utilities/v1/shieldUtilities'
 
 export function updateBasicShieldInfoUtility(character: CharacterVersion1, key: ShieldInfoObjectKeys, value: string | number) {
     return {
@@ -18,6 +19,33 @@ export function updateBasicShieldInfoUtility(character: CharacterVersion1, key: 
 }
 
 export function updateShieldModifierUtility(character: CharacterVersion1, modifier: ShieldModifiersInfoKeys, key: ShieldModifiersObjectKeys, value: number) {
+    let newModifiers = {
+        ...character.pageTwoInfo.combatWorkspaceInfo.shieldInfo.modifiers,
+        [modifier]: {
+            ...character.pageTwoInfo.combatWorkspaceInfo.shieldInfo.modifiers[modifier],
+            [key]: value
+        }
+    }
+
+    newModifiers = {
+        def: {
+            ...newModifiers.def,
+            total: calculateShieldDefense(newModifiers.def.base, newModifiers.def.misc)
+        },
+        fat: {
+            ...newModifiers.fat,
+            total: calculateShieldFatigue(newModifiers.fat.base, newModifiers.fat.skill, newModifiers.fat.misc)
+        },
+        pry: {
+            ...newModifiers.pry,
+            total: calculateShieldParry(newModifiers.pry.base, newModifiers.pry.skill, newModifiers.pry.misc)
+        },
+        brk: {
+            ...newModifiers.brk,
+            total: calculateShieldBreakage(newModifiers.brk.base, newModifiers.brk.skill, newModifiers.brk.misc)
+        }
+    }
+
     return {
         ...character,
         pageTwoInfo: {
@@ -26,13 +54,7 @@ export function updateShieldModifierUtility(character: CharacterVersion1, modifi
                 ...character.pageTwoInfo.combatWorkspaceInfo,
                 shieldInfo: {
                     ...character.pageTwoInfo.combatWorkspaceInfo.shieldInfo,
-                    modifiers: {
-                        ...character.pageTwoInfo.combatWorkspaceInfo.shieldInfo.modifiers,
-                        [modifier]: {
-                            ...character.pageTwoInfo.combatWorkspaceInfo.shieldInfo.modifiers[modifier],
-                            [key]: value
-                        }
-                    }
+                    modifiers: newModifiers
                 }
             }
         }
