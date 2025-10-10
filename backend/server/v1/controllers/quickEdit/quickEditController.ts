31d226f97670d9006937d5685bf00ff5511bd3c9
Trent <mr.peschke@gmail.com>
2025-10-10 16:36:23 -0600

    gating quick edit to the owner of the character

--- backend/server/v1/controllers/quickEdit/quickEditController.ts
+++ backend/server/v1/controllers/quickEdit/quickEditController.ts
@@ -5,6 +5,8 @@ import { saveUnspentCrp } from './utilities/crpUnspent'
 import { saveCrpSpent } from './utilities/crpSpent'
 import { saveIntegrity } from './utilities/integrity'
 import { saveGritDice } from './utilities/gritDice'
+import query from '../../../db/database'
+import userSQL from '../../queries/user'
 
 interface EditRequest extends Request {
     params: {
@@ -15,54 +17,60 @@ interface EditRequest extends Request {
 
 export async function quickEditCharacter(request: EditRequest, response: Response) {
     const { characterID, attribute, value } = request.body
-// TODO check character ownership
-    let sendSuccess = false
 
-    switch (attribute) {
-        case 'crpUnspent':
-            sendSuccess = await saveUnspentCrp(characterID, value)
-            break
-        case 'crpSpent':
-            sendSuccess = await saveCrpSpent(characterID, value)
-            break
-        case 'integrity':
-            sendSuccess = await saveIntegrity(characterID, value)
-            break
-        case 'gritDice':
-            sendSuccess = await saveGritDice(characterID, value)
-            break
-        case 'favor':
-            break
-        case 'stress':
-            break
-        case 'relaxation':
-            break
-        case 'copper':
-            break
-        case 'silver':
-            break
-        case 'gold':
-            break
-        case 'platinum':
-            break
-        case 'assets':
-            break
-        case 'notes':
-            break
-        default:
+    const [{ userid: userIDFromDB }] = await query(userSQL.getCharacterUserID, characterID)
+    
+    if (userIDFromDB === request.user?.id) {
+        let sendSuccess = false
+
+        switch (attribute) {
+            case 'crpUnspent':
+                sendSuccess = await saveUnspentCrp(characterID, value)
+                break
+            case 'crpSpent':
+                sendSuccess = await saveCrpSpent(characterID, value)
+                break
+            case 'integrity':
+                sendSuccess = await saveIntegrity(characterID, value)
+                break
+            case 'gritDice':
+                sendSuccess = await saveGritDice(characterID, value)
+                break
+            case 'favor':
+                break
+            case 'stress':
+                break
+            case 'relaxation':
+                break
+            case 'copper':
+                break
+            case 'silver':
+                break
+            case 'gold':
+                break
+            case 'platinum':
+                break
+            case 'assets':
+                break
+            case 'notes':
+                break
+            default:
+                checkForContentTypeBeforeSending(response, { success: false })
+        }
+
+        // wounds
+        // equipment
+
+        // armor misc modifiers
+        // shield misc modifiers
+        // weapon misc modifiers
+
+        if (sendSuccess) {
+            checkForContentTypeBeforeSending(response, { success: true })
+        } else {
             checkForContentTypeBeforeSending(response, { success: false })
-    }
-
-    // wounds
-    // equipment
-
-    // armor misc modifiers
-    // shield misc modifiers
-    // weapon misc modifiers
-
-    if (sendSuccess) {
-        checkForContentTypeBeforeSending(response, { success: true })
+        }
     } else {
-        checkForContentTypeBeforeSending(response, { success: false })
+        checkForContentTypeBeforeSending(response, { success: false, message: "You Don't Own This Character" })
     }
 }
