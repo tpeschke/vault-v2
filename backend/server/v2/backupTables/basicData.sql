ad16b65baf0043cb63927bceb8c9351957292aeb
peschke <mr.peschke@gmail.com>
2025-11-23 17:16:58 -0700

    doing a bit of refactoring with the backup tables

---
+++ backend/server/v2/backupTables/basicData.sql
@@ -0,0 +1,10 @@
+create table
+	v2characterowner (characterid serial primary key, ownerid integer);
+
+create table
+	v2characterpages (
+		id serial primary key,
+		index integer,
+		pagetypeid integer,
+		characterid integer
+	);
